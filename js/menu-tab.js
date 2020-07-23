6dc048d79b7e27c52419e63c4ca291b76e4a52d5
jjub0217 <noonssub1004@gmail.com>
2020-07-23 16:30:27 +0900

    공지사항 및 자료실 영역 레이아웃 및 기능구현

---
+++ js/menu-tab.js
@@ -0,0 +1,27 @@
+		// DOM 노드 찾기
+		// const panel = document.getElementsByClassName('board section');
+		// const tab = document.querySelector('tab');
+		
+		var items = $('.menu-item');
+		var btn = $('.btn-menu');
+		var panel = $('.board section');
+		var tab = $('.tab');
+		
+		// 기능 구현
+
+		btn.click(function(e){
+			e.preventDefault();
+			items.removeClass('menu-act');
+			$(this).parent().addClass('menu-act');
+		})
+
+
+		tab.click(function(e) {
+
+			e.preventDefault();
+			panel.removeClass('tab-act');
+			$(this).parent().parent().addClass('tab-act');
+		
+        });
+        
+        
